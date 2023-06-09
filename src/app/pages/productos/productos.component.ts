21339d6ecf77f14d4f4f124c8dec143c6fc45438
Jonatan Fajardo <69583436+JonatanFajardo@users.noreply.github.com>
2023-06-09 06:25:45 -0600

    Agregado de estilos.

--- src/app/pages/productos/productos.component.ts
+++ src/app/pages/productos/productos.component.ts
@@ -10,22 +10,25 @@ import { ProductosService } from 'src/app/services/productos/productos.service';
   styleUrls: ['./productos.component.scss'],
 })
 export class ProductosComponent implements OnInit {
+  precioMinimo = 0;
+  precioMaximo = 10000;
+
   productos: any;
   public page!: number;
   marcas: any;
   productosFiltrados: any;
 
   constructor(
-    private router:Router,
+    private router: Router,
     private productosService: ProductosService,
-    private marcasService: MarcaService,
+    private marcasService: MarcaService
   ) {
     this.ObtenerProductosCards();
     this.ObtenerMarcas();
   }
+  ngOnInit(): void {}
 
   ObtenerProductosCards() {
-    // debugger
     this.productosService.ObtenerProductosCards().subscribe((datos) => {
       this.productos = datos;
       this.productosFiltrados = datos;
@@ -34,27 +37,48 @@ export class ProductosComponent implements OnInit {
   }
 
   ObtenerMarcas() {
-    // debugger
     this.marcasService.ObtenerMarcas().subscribe((datos) => {
       this.marcas = datos;
       console.log(this.marcas);
     });
   }
+  
   filtrarProductos() {
-    this.productosFiltrados = this.productos.filter((producto: any) => {
-      return this.marcas.some(
-        (marca: any) => marca.checked && producto.marc_Id === marca.marc_Id
-      );
-    });
+    this.productosFiltrados = this.productos.filter(
+      (producto: any) =>
+        producto.prod_Precio >= this.precioMinimo &&
+        producto.prod_Precio <= this.precioMaximo
+    );
+
+    // Verifica si alguna marca está seleccionada
+    const hayMarcasSeleccionadas = this.marcas.some(
+      (marca: any) => marca.checked
+    );
+
+    // Filtra los productos en base a las marcas seleccionadas
+    this.productosFiltrados = this.productosFiltrados.filter(
+      (producto: any) => {
+        // Si no hay marcas seleccionadas, no filtra ningún producto
+        if (!hayMarcasSeleccionadas) {
+          return true;
+        }
+        // Si hay marcas seleccionadas, filtra solo los productos que corresponden a esas marcas
+        return this.marcas.some(
+          (marca: any) => marca.checked && producto.marc_Id === marca.marc_Id
+        );
+      }
+    );
   }
-  pageChange($event:any){
-    this.page = $event
+
+  filtradoPrecio() {}
+  pageChange($event: any) {
+    this.page = $event;
     window.onload = () => {
       window.scrollTo(0, 0);
-    }
+    };
   }
-  redirectToProductosDetalle(id:number){
-    this.router.navigateByUrl("productos-detalle/"+id)
+
+  redirectToProductosDetalle(id: number) {
+    this.router.navigateByUrl('productos-detalle/' + id);
   }
-  ngOnInit(): void {}
 }
